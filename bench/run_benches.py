cc38415ed13a81885b6366aa0042bb7bf8bcf7bb
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-06-04 13:34:29 +0200

    Append to output file incrementally, not all at the end. Removed Safari.

--- bench/run_benches.py
+++ bench/run_benches.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
@@ -7,25 +9,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-try:
-    from selenium.webdriver.safari.options import Options as SafariOptions
-    safari_supported = True
-except:
-    safari_supported = False
 
 import itertools
 
 impls = [
-    "Js",
     "Rust",
     "Cheerp",
+    "Js",
 ]
 browsers = [
     "Chrome",
     "Firefox",
 ]
-if safari_supported:
-    browsers.append("Safari")
 benches = [
     "set.first.breakpoint",
     "first.pause.at.exception",
@@ -42,51 +37,54 @@ sources_and_factors = [
     ("SCALA_JS_RUNTIME_SOURCE_MAP",2),
 ]
 
-data = "Implementation,Browser,Mappings.Size,Operation,Time"
 
 headless = True
 runs = 100
-for impl,browser,bench,(source,factor) in itertools.product(impls,browsers,benches,sources_and_factors):
-    print("Running "+str((impl,browser,bench,source,factor)))
-    if browser == "Firefox":
-        options = FirefoxOptions()
-        options.set_headless(headless)
-        driver = webdriver.Firefox(firefox_options=options)
-    elif browser == "Chrome":
-        options = ChromeOptions()
-        options.set_headless(headless)
-        driver = webdriver.Chrome(chrome_options=options)
-    elif browser == "Safari":
-        options = SafariOptions()
-        options.set_headless(headless)
-        driver = webdriver.Chrome(safari_options=options)
-    driver.get("http://localhost:8000/bench/bench.html")
-    iters = driver.find_element_by_id("iters")
-    iters.clear()
-    iters.send_keys(str(runs))
-    select_map = Select(driver.find_element_by_id("input-map"))
-    select_map.select_by_value(source)
-    multiply = driver.find_element_by_id("multiply-size-by")
-    multiply.clear()
-    multiply.send_keys(str(factor))
-    impl_and_browser = driver.find_element_by_id("impl-and-browser")
-    impl_and_browser.clear()
-    impl_and_browser.send_keys(impl+","+browser)
-    select_impl = Select(driver.find_element_by_id("impl"))
-    select_impl.select_by_value(impl)
-    button = driver.find_element_by_id(bench)
-    button.click()
 
-    element = WebDriverWait(driver,60*100).until(
-        EC.presence_of_element_located((By.ID, "data."+bench))
-    )
+empty_file = True
+try:
+    if os.path.getsize("data.csv") > 0:
+        empty_file = False
+except:
+    pass
+with open("data.csv","a") as f:
+    if empty_file:
+        f.write("Implementation,Browser,Mappings.Size,Operation,Time")
+    for impl,browser,bench,(source,factor) in itertools.product(impls,browsers,benches,sources_and_factors):
+        print("Running "+str((impl,browser,bench,source,factor)))
+        if browser == "Firefox":
+            options = FirefoxOptions()
+            options.set_headless(headless)
+            driver = webdriver.Firefox(firefox_options=options)
+        elif browser == "Chrome":
+            options = ChromeOptions()
+            options.set_headless(headless)
+            driver = webdriver.Chrome(chrome_options=options)
+        driver.get("http://localhost:8000/bench/bench.html")
+        iters = driver.find_element_by_id("iters")
+        iters.clear()
+        iters.send_keys(str(runs))
+        select_map = Select(driver.find_element_by_id("input-map"))
+        select_map.select_by_value(source)
+        multiply = driver.find_element_by_id("multiply-size-by")
+        multiply.clear()
+        multiply.send_keys(str(factor))
+        impl_and_browser = driver.find_element_by_id("impl-and-browser")
+        impl_and_browser.clear()
+        impl_and_browser.send_keys(impl+","+browser)
+        select_impl = Select(driver.find_element_by_id("impl"))
+        select_impl.select_by_value(impl)
+        button = driver.find_element_by_id(bench)
+        button.click()
 
-    data += "\n" + element.text
+        element = WebDriverWait(driver,60*100).until(
+            EC.presence_of_element_located((By.ID, "data."+bench))
+        )
 
-    mean = driver.find_elements_by_tag_name("td")[6]
-    print('Done! mean: '+mean.text)
+        f.write("\n")
+        f.write(element.text)
 
-    driver.close()
-with open("data.csv","w") as f:
-    f.write(data)
+        mean = driver.find_elements_by_tag_name("td")[6]
+        print('Done! mean: '+mean.text)
 
+        driver.quit()
